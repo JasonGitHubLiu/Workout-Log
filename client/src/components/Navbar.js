2151edf0648c6e0f7bc02a8976dddeb8851ab86d
JasonGitHubLiu <jason.liu002@umb.edu>
2023-03-29 16:54:01 -0400

    Added navbar and background picture

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,64 +1,60 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Navbar({ user, setUser }) {
-  
   const logout = () => {
-    localStorage.removeItem("token")
-    setUser({})
+    localStorage.removeItem('token');
+    setUser({});
   };
 
   return (
     <nav
-    class="navbar navbar-expand-lg bg-body-tertiary bg-dark "
-    data-bs-theme="dark"
-  >
-    <div class="container-fluid w-100">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="user-auth">
-      {user ? 
-        <>
-          
-
-
-          <li style={{ color: "black" }}>Welcome {user}!</li>
-          <li className="posts-nav">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="posts-nav">
-            <Link to="/posts">Posts</Link>
-          </li>
-          <li onClick={logout}>
-            <Link to="/login">Logout</Link>
-          </li>
-        </>
-       : 
-        <>
-          <li className="posts-nav">
-            <Link to="/posts">Posts</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </>
-      }
-    </ul>
-    </div>
+      class="navbar navbar-expand-lg bg-body-tertiary bg-dark "
+      data-bs-theme="dark"
+    >
+      <div class="container-fluid ">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="user-auth">
+            {user ? (
+              <>
+                <li className="posts-nav">
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/posts">Posts</Link>
+                </li>
+                <b><li style={{ color: 'Red' }}>Welcome {user}!</li></b>
+                <li onClick={logout}>
+                  <Link to="/login">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="posts-nav">
+                  <Link to="/posts">Posts</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
 
