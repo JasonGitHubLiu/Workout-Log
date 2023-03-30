62c49f754737c1ed33b8da5472e474dbef03cbf8
JasonGitHubLiu <jason.liu002@umb.edu>
2023-03-30 11:58:50 -0400

    Added to show view

--- client/src/pages/posts/Show.js
+++ client/src/pages/posts/Show.js
@@ -54,7 +54,10 @@ function Show({ user }) {
                 <br></br>
                 <h1>Show View</h1>
                 <div className="a-post">
-                    <h2>{post.exercise}</h2>
+                    <h3>Exercise: {post.exercise}</h3>
+                    <h3>Weight: {post.weight}</h3>
+                    <h3>Sets x Reps: {post.sxr}</h3>
+                    <h3>RPE: {post.rpe}</h3>
                     <h5 style={{ opacity: '.3'}}>Posted by {post.user} on {new Date(post.createdAt).toLocaleDateString()} at {new Date(post.createdAt).toLocaleTimeString()}</h5>
                     <div className='p-body'>{post.body}</div><br /><br />
 
