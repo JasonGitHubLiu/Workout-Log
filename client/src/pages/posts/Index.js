aa8301f8f205c81d3de575f52584e85484219c4c
JasonGitHubLiu <jason.liu002@umb.edu>
2023-03-29 23:22:16 -0400

    working on index table

--- client/src/pages/posts/Index.js
+++ client/src/pages/posts/Index.js
@@ -4,25 +4,60 @@ import { getAllPosts } from "../../services/postService"
 
 function Index({ user }) {
 
-    const [posts, setPosts] = useState([])
+    const [workout, setWorkOut] = useState([])
 
     useEffect(() => {
         async function loadData() {
             const data = await getAllPosts()
-            setPosts(data)
+            setWorkOut(data)
         }
         loadData()
     }, [])
-    console.log(posts)
+    console.log(workout)
     return (
             <div>
                 <h1>Index View</h1>
                 <div id="posts">
-
-                        {posts?.map((post, index) => 
-                            <Link to={`/posts/${post._id}`} key={index}>
-                                <div className="a-post">
-                                    {post.exercise}
+                <table
+          class="table table-primary table-striped text-center "
+          style={{ width: '60vw' }}
+        >
+          <thead>
+            <tr>
+              <th scope="col">Exercise</th>
+              <th scope="col">Weight</th>
+              <th scope="col">Sets x Reps</th>
+              <th scope="col">RPE</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+            {workout.map((x) => (
+              <>
+                <tr>
+                  <th scope="row">{x.airport}</th>
+                  <td>{x?.exercise}</td>
+                  <td>
+                    <form
+                      action={`/flights/${workout._id}/destinations/${x._id}?_method=DELETE`}
+                      method="POST"
+                    >
+                      <button className="btn btn-outline-danger mx-5">
+                        Delete
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+                        {workout?.map((x, index) => 
+                            <Link to={`/posts/${x._id}`} key={index}>
+                                <div >
+                                Exercise:
+                                    {x.exercise}
                                 </div>
                             </Link>
                         )}
