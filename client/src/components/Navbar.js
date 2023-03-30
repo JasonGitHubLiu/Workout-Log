3848de85a6acc48fcf58adc6d1bf0570f9fb336e
JasonGitHubLiu <jason.liu002@umb.edu>
2023-03-30 15:33:01 -0400

    Fixed Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,55 +7,123 @@ function Navbar({ user, setUser }) {
   };
 
   return (
-    <nav
-      class="navbar navbar-expand-lg bg-body-tertiary bg-dark "
-      data-bs-theme="dark"
-    >
-      <div class="container-fluid ">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="user-auth">
-            {user ? (
-              <>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/posts">Posts</Link>
-                </li>
-                <b><li style={{ color: 'Red' }}>Welcome {user}!</li></b>
-                <li onClick={logout}>
-                  <Link to="/login">Logout</Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/posts">Posts</Link>
-                </li>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+<div className="container-fluid">
+    <Link className="navbar-brand" to="/">Home</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+     
+         
+                    {user ? (
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/posts">Posts</Link>
+          </li>
+          <b><li style={{ color: 'Red' }}>Welcome {user}!</li></b>
+          
+          <li onClick={logout}>
+            <Link className="nav-link" to="/login">Logout</Link>
+          </li>
+        </>
+      ) : (
+        <>
+          <li className="nav-item">
+            <Link className="nav-link" to="/posts">Posts</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">Register</Link>
+          </li>
+        </>
+      )}
+
+        </ul>
+    </div>
+</div>
+</nav>
   );
 }
 
 export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Link } from 'react-router-dom';
+
+// function Navbar({ user, setUser }) {
+//   const logout = () => {
+//     localStorage.removeItem('token');
+//     setUser({});
+//   };
+
+//   return (
+//     <nav
+//       className="navbar navbar-expand-lg bg-body-tertiary bg-dark "
+//       data-bs-theme="dark"
+//     >
+//       <div className="container-fluid ">
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarSupportedContent"
+//           aria-controls="navbarSupportedContent"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="user-auth">
+//             {user ? (
+//               <>
+//                 <li>
+//                   <Link to="/">Home</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/posts">Posts</Link>
+//                 </li>
+//                 <b><li style={{ color: 'Red' }}>Welcome {user}!</li></b>
+//                 <li onClick={logout}>
+//                   <Link to="/login">Logout</Link>
+//                 </li>
+//               </>
+//             ) : (
+//               <>
+//                 <li>
+//                   <Link to="/posts">Posts</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/login">Login</Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/register">Register</Link>
+//                 </li>
+//               </>
+//             )}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
     <>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div className="bg-white text-black opacity-50 text-center">
         <br />
         <h1>Hello Home </h1>
@@ -9,10 +34,8 @@ export default function Home() {
 
         <h2>
           Log your sets, weight, and reps for your exercises in order to push
-          yourself each time you visit the gym. Store your body weight to keep
-          yourself motivated. Switch easily between different workout routines
-          and days depending on your needs or fitness goals. Watch your lifts
-          increase showing weight for each rep and total weight for the set.
+          yourself each time you visit the gym. Watch your lifts increase
+          showing weight for each rep and total weight for the set.
         </h2>
         <br />
         <br />
